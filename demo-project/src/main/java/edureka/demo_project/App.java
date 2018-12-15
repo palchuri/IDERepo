3763d140f2675a5ab2af13ab6618ae664bb1304a
edureka <edureka@master>
2018-12-15 22:37:20 +0530

    IDE Commit

---
+++ demo-project/src/main/java/edureka/demo_project/App.java
@@ -0,0 +1,13 @@
+package edureka.demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 123!" );
+    }
+}
